c6d07373b7d6cc72fd9ef42dc2caa3606f27e843
OpenShift guest <support@labjack.com>
2014-02-15 12:53:23 -0500

    Incremental JSON update.

--- save_changes.py
+++ save_changes.py
@@ -2,13 +2,15 @@ import json
 import os
 import subprocess
 
+import ljmmm
+
 print 'Checking JSON file...'
 json_file_path = os.path.join('LabJack', 'LJM', 'ljm_constants.json')
 with open(json_file_path) as f:
     try:
-        json.load(f)
-    except:
-        print '[ERROR] JSON file could not be parsed. Please check.'
+        ljmmm.get_raw_registers_data(json_file_path)
+    except Exception as e:
+        print '[ERROR] JSON file could not be parsed. Please check. (' + str(e) + ')'
         exit(1)
 
 print 'Saving to Git repository...'
