c1a42d443783243ea654a9c42fb0f83cf4342e17
OpenShift guest <support@labjack.com>
2014-04-25 20:27:36 -0400

    Removing WIFI tag from LED regs.

--- save_changes.py
+++ save_changes.py
@@ -1,8 +1,17 @@
 import json
 import os
 import subprocess
+import sys
 
 import ljmmm
+
+if len(sys.argv) > 2:
+    print 'Too many args. Commit message may be arg 0.'
+    sys.exit(1)
+    
+commit_message = "Incremental JSON update."
+if len(sys.argv) == 2:
+    commit_message = sys.argv[1]
 
 print 'Checking JSON file...'
 json_file_path = os.path.join('LabJack', 'LJM', 'ljm_constants.json')
@@ -28,6 +37,6 @@ for device in json_map:
 
 print 'Saving to Git repository...'
 subprocess.check_call('git pull', shell=True)
-subprocess.call('git commit -a -m "Incremental JSON update."', shell=True)
+subprocess.call('git commit -a -m "%s"' % commit_message, shell=True)
 subprocess.call('git push', shell=True)
 print 'Finished!'
