788843881556ff2d8c11ef7fc1797c67496927ce
Rory Olsen <roris.solaris@gmail.com>
2016-05-18 16:06:25 +0100

    Adding check for Digit constant in generated map.

--- gen_test/test_c_header.c
+++ gen_test/test_c_header.c
@@ -4,5 +4,9 @@
 #include "LabJackMModbusMap.h"
 
 int main() {
+    // Digit-specific: does it exist?
+    int digit = LJM_DGT_TEMPERATURE_LATEST_RAW_ADDRESS;
+
+    // T7-specific: does it exist?
     return LJM_AIN0_ADDRESS;
 }
